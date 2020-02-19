9ad05009e3dcf0cbbaf475a332a37c8888518262
Max Winokan <mwinokan@me.com>
2020-02-19 10:27:50 +0000

    Dependency loading via lmod (called via MPyTools/module).

--- asemolplot/__init__.py
+++ asemolplot/__init__.py
@@ -1,9 +1,16 @@
+import mout # https://github.com/mwinokan/MPyTools
+import module # https://github.com/mwinokan/MPyTools
 
 # load the correct modules for ASE/PoV-Ray
-import os
-import mout # https://github.com/mwinokan/MPyTools
-os.system('source $MWAMPPATH/load_pov.sh 2> /dev/null')
-mout.out("ASE & PoV-Ray dependencies loaded.",printScript=True,)
+module.module('--expert','load','Boost/1.63.0-intel-2017a-Python-2.7.13')
+module.module('--expert','load','zlib/1.2.8-intel-2016a')
+module.module('--expert','load','libpng/1.6.24-intel-2016a')
+module.module('--expert','load','libjpeg-turbo/1.5.0-intel-2016a')
+module.module('--expert','load','LibTIFF/4.0.6-intel-2016a')
+module.module('--expert','load','anaconda3/2019.03')
+
+mout.out("PoV-Ray dependencies loaded.",
+         printScript=True,)
 
 # import functions
 from .povplot import makePovImage
