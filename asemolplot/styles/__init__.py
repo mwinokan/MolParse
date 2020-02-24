d2535636ab7c0ce84b8fff94e41a18e8315c4060
Max Winokan <mwinokan@me.com>
2020-02-24 16:07:54 +0000

    Added standard_rot, and standard_bonds styles.

--- asemolplot/styles/__init__.py
+++ asemolplot/styles/__init__.py
@@ -1,3 +1,5 @@
 from .styles import standard
+from .styles import standard_rot
 from .styles import standard_cell
+from .styles import standard_bonds
 from .styles import gif_standard
--- asemolplot/styles/styles.py
+++ asemolplot/styles/styles.py
@@ -1,13 +1,23 @@
 standard={"canvas_width":2000, 
           "drawCell":False, 
           "radii":0.9, 
+          "rotation":"", 
+          "celllinewidth":0.0} 
+
+standard_rot={"canvas_width":2000, 
+          "drawCell":False, 
+          "radii":0.9, 
           "rotation":"-90x,65z", 
+          "celllinewidth":0.0} 
+
+standard_bonds={"canvas_width":2000, 
+          "drawCell":False, 
+          "radii":0.5, 
           "celllinewidth":0.0} 
 
 standard_cell={"canvas_width":2000, 
                "drawCell":True, 
                "radii":0.9, 
-               "rotation":"-90x,65z", 
                "celllinewidth":0.07} 
 
 gif_standard={"loop":0,
