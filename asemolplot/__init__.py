01da3fa7353eac20c3be3a6202b3f9b0d3ec64d1
Max Winokan <mwinokan@me.com>
2020-02-24 16:04:27 +0000

    Added read(), write(), and loadPov().

--- asemolplot/__init__.py
+++ asemolplot/__init__.py
@@ -1,11 +1,18 @@
 import mout # https://github.com/mwinokan/MPyTools
 
 # import functions
+
+from .io import read
+from .io import write
+
 from .plot import makeImage
 from .plot import makeImages
 from .plot import makeAnimation
+
+from .povplot import loadPov
 from .povplot import makePovImage
 from .povplot import makePovImages
 from .povplot import makePovAnimation
 from .povplot import crop
+
 from . import styles
