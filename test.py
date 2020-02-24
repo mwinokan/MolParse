3855d3c6eab91ff2e25fbdc6fc337a6ddd73132c
Max Winokan <mwinokan@me.com>
2020-02-24 16:08:57 +0000

    Updated example.

--- test.py
+++ test.py
@@ -4,6 +4,14 @@ import mcol              # https://github.com/mwinokan/MPyTools
 import mout              # https://github.com/mwinokan/MPyTools
 import asemolplot as amp # https://github.com/mwinokan/AseMolPlot
 
-atoms = io.read('traj.traj', index=-10)
+# load some molecule
+from ase.data.pubchem import pubchem_atoms_search, pubchem_atoms_conformer_search
+atoms = pubchem_atoms_search(smiles='N1CCN(CC1)C(C(F)=C2)=CC(=C2C4=O)N(C3CC3)C=C4C(=O)O')
 
-amp.makePovImage('test',atoms,**amp.styles.standard)
+custom_style = amp.styles.standard
+custom_style['rotation'] = ''
+custom_style['canvas_width'] = 500
+
+# make some single images:
+amp.makeImage('amp',atoms,**amp.styles.standard)
+amp.makePovImage('pov',atoms,**amp.styles.standard)
