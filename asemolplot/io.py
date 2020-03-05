ca4024d2bbd803ae76ade5112222e0d3d3d6bf6b
Max Winokan <mwinokan@me.com>
2020-03-05 11:31:30 +0000

    Wrappers for ase.io read() and write() functions.

---
+++ asemolplot/io.py
@@ -0,0 +1,34 @@
+from ase import io
+
+import mcol # https://github.com/mwinokan/MPyTools
+import mout # https://github.com/mwinokan/MPyTools
+
+def write(filename,image,verbosity=1,**parameters):
+
+  if (verbosity > 0):
+    mout.out("writing "+mcol.file+
+             filename+
+             mcol.clear+" ... ",
+             printScript=True,
+             end='') # user output
+
+  io.write(filename,image,**parameters)
+
+  if (verbosity > 0):
+    mout.out("Done.") # user output
+
+def read(filename,index=None,verbosity=1,**parameters):
+
+  if (verbosity > 0):
+    mout.out("reading "+mcol.file+
+             filename+
+             mcol.clear+" ... ",
+             printScript=True,
+             end='') # user output
+
+  atoms = io.read(filename,index,**parameters)
+
+  if (verbosity > 0):
+    mout.out("Done.") # user output
+
+  return atoms
