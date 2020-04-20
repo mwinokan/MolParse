59cbf524cb0d9ee62e71f5ef3355c23a5c0ee1b2
Max Winokan <mwinokan@me.com>
2020-04-20 14:05:32 +0100

    Added printScript argument.

--- asemolplot/io.py
+++ asemolplot/io.py
@@ -3,13 +3,13 @@ from ase import io
 import mcol # https://github.com/mwinokan/MPyTools
 import mout # https://github.com/mwinokan/MPyTools
 
-def write(filename,image,verbosity=1,**parameters):
+def write(filename,image,verbosity=1,printScript=False,**parameters):
 
   if (verbosity > 0):
     mout.out("writing "+mcol.file+
              filename+
              mcol.clear+" ... ",
-             printScript=True,
+             printScript=printScript,
              end='') # user output
 
   io.write(filename,image,**parameters)
@@ -17,13 +17,13 @@ def write(filename,image,verbosity=1,**parameters):
   if (verbosity > 0):
     mout.out("Done.") # user output
 
-def read(filename,index=None,verbosity=1,**parameters):
+def read(filename,index=None,printScript=False,verbosity=1,**parameters):
 
   if (verbosity > 0):
     mout.out("reading "+mcol.file+
              filename+
              mcol.clear+" ... ",
-             printScript=True,
+             printScript=printScript,
              end='') # user output
 
   atoms = io.read(filename,index,**parameters)
