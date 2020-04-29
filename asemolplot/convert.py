3057455c97e9b107ef0d225db9ab702edf88351e
mwinokan <mwinokan@me.com>
2020-04-29 11:19:46 +0200

    Tagging support for single image GRO

--- asemolplot/convert.py
+++ asemolplot/convert.py
@@ -78,7 +78,37 @@ def gro2traj(input,output,verbosity=1,printScript=False,tagging=True):
     if (verbosity > 1):
       mout.out("")
 
+  if tagging:
+      # initialise arrays
+      taglist=[]
+
+      if (verbosity > 1):
+        mout.out("parsing GRO for tags ... ",printScript=printScript,end='')
+
+      line_number = 0
+      max_line_number = 1000
+
+      # Parse the GRO to get the tags
+      with open(input,"r") as input_gro:
+        for line in input_gro:
+          if line_number == 1:
+            max_line_number = int(line.strip().split()[0]) + 2
+          if line_number == max_line_number:
+            break
+          if line_number > 1:
+            taglist.append(int(''.join(filter(lambda i: i.isdigit(), line.strip().split()[1]))))
+          line_number = line_number + 1
+
+      if (verbosity > 1):
+        mout.out("Done.")
+
   in_traj = read(input,index=":",verbosity=verbosity-1)
+
+  if tagging:
+      # set the tags
+      for atoms in in_traj:
+        for index,tag in enumerate(taglist):
+          atoms[index].tag = tag
 
   write(output,in_traj,verbosity=verbosity-1)
 
