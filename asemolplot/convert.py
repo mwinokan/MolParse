b1b49f35caf08a0db2e2098a35aa42ee181b0966
mwinokan <mwinokan@me.com>
2020-07-16 15:39:29 +0200

    tagging now handled in io

--- asemolplot/convert.py
+++ asemolplot/convert.py
@@ -22,39 +22,7 @@ def pdb2traj(input,output,verbosity=1,printScript=False,tagging=True):
     if (verbosity > 1):
       mout.out("")
 
-    if tagging:
-      # initialise arrays
-      taglist=[]
-
-      if (verbosity > 1):
-        mout.out("parsing PDB for tags ... ",printScript=printScript,end='')
-
-      # Parse the first model in the PDB to get the tags
-      with open(input,"r") as input_pdb:
-        searching = True
-        for line in input_pdb:
-          if searching:
-            if line.startswith("MODEL"):
-              searching = False
-          else:
-            if line.startswith("ENDMDL"):
-              break
-            if line.startswith("TER"):
-              continue
-            else:
-              # get the tags:
-              taglist.append(int(''.join(filter(lambda i: i.isdigit(), line.strip().split()[2]))))
-
-      if (verbosity > 1):
-        mout.out("Done.")
-
     in_traj = read(input,index=":",verbosity=verbosity-1)
-
-    if tagging:
-      # set the tags
-      for atoms in in_traj:
-        for index,tag in enumerate(taglist):
-          atoms[index].tag = tag
 
     write(output,in_traj,verbosity=verbosity-1)
 
