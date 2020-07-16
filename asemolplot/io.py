f98d7c37bb58b2b005cbed2bf06282b34c987b3c
mwinokan <mwinokan@me.com>
2020-07-16 15:39:53 +0200

    More solid tagging support

--- asemolplot/io.py
+++ asemolplot/io.py
@@ -1,4 +1,5 @@
 from ase import io
+from ase import atoms as aseatoms
 
 import mcol # https://github.com/mwinokan/MPyTools
 import mout # https://github.com/mwinokan/MPyTools
@@ -17,7 +18,7 @@ def write(filename,image,verbosity=1,printScript=False,**parameters):
   if (verbosity > 0):
     mout.out("Done.") # user output
 
-def read(filename,index=None,printScript=False,verbosity=1,**parameters):
+def read(filename,index=None,printScript=False,verbosity=1,tagging=True,tagByResidue=False,**parameters):
 
   if (verbosity > 0):
     mout.out("reading "+mcol.file+
@@ -28,7 +29,52 @@ def read(filename,index=None,printScript=False,verbosity=1,**parameters):
 
   atoms = io.read(filename,index,**parameters)
 
+  if tagging and filename.endswith(".pdb"):
+    getAndSetTags(filename,atoms,byResidue=tagByResidue)
+
   if (verbosity > 0):
     mout.out("Done.") # user output
 
   return atoms
+
+def getAndSetTags(pdb,atoms,byResidue=False):
+
+  # Parse the first model in the PDB to get the tags
+  taglist=[]
+  with open(pdb,"r") as input_pdb:
+    searching = True
+    for line in input_pdb:
+      if searching:
+        if line.startswith("MODEL"):
+          searching = False
+        elif line.startswith("ATOM"):
+          searching = False
+          taglist.append(tagFromLine(line,byResidue=byResidue))
+      else:
+        if line.startswith("ENDMDL"):
+          break
+        if line.startswith("END"):
+          break
+        if line.startswith("TER"):
+          continue
+        else:
+          # get the tags:
+          taglist.append(tagFromLine(line,byResidue=byResidue))
+
+  # set the tags
+  if isinstance(atoms,aseatoms.Atoms):
+    for index,tag in enumerate(taglist):
+      atoms[index].tag = tag
+  else:
+    for atms in atoms:
+      for index,tag in enumerate(taglist):
+        atms[index].tag = tag
+
+def tagFromLine(line,byResidue):
+  try:
+    if byResidue:
+      return int(line[24:27])
+    else:
+      return int(''.join(filter(lambda i: i.isdigit(), line.strip().split()[2])))
+  except:
+    return 0
