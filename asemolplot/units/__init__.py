c98eff14c2559c4a4ef2ff8dd32fa239261b21bf
Max Winokan <mwinokan@me.com>
2020-07-23 15:30:59 +0100

    Some units and constants

---
+++ asemolplot/units/__init__.py
@@ -0,0 +1,14 @@
+
+# Constants
+mol = 6.02214076e23
+
+# Units in SI
+e = 1.602176634e-19 # C
+eV = 1.602176634e-19 # J
+Angstrom = 1e-10 # m
+AA = Angstrom # m
+kcal = 4184 # J
+
+# Unit Prefixes
+kilo = 1e3
+nano = 1e-9
