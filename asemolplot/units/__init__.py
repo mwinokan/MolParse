bcd84de5092bc55ae08156051be625b89b85925c
Max Winokan <mwinokan@me.com>
2020-07-24 10:45:19 +0100

    pi and rad

--- asemolplot/units/__init__.py
+++ asemolplot/units/__init__.py
@@ -1,6 +1,7 @@
 
 # Constants
 mol = 6.02214076e23
+pi = 3.141592653589793238462
 
 # Units in SI
 e = 1.602176634e-19 # C
@@ -8,6 +9,7 @@ eV = 1.602176634e-19 # J
 Angstrom = 1e-10 # m
 AA = Angstrom # m
 kcal = 4184 # J
+rad = 180/pi # degrees
 
 # Unit Prefixes
 kilo = 1e3
