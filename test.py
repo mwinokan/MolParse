718f7c9b28d2b52734640ab0e03f80ee8ac1529a
Max Winokan <mwinokan@me.com>
2022-01-24 14:18:55 +0000

    cs4qb.buildGulpFF test

--- test.py
+++ test.py
@@ -4,14 +4,25 @@ import mcol              # https://github.com/mwinokan/MPyTools
 import mout              # https://github.com/mwinokan/MPyTools
 import asemolplot as amp # https://github.com/mwinokan/AseMolPlot
 
-# load some molecule
-from ase.data.pubchem import pubchem_atoms_search, pubchem_atoms_conformer_search
-atoms = pubchem_atoms_search(smiles='N1CCN(CC1)C(C(F)=C2)=CC(=C2C4=O)N(C3CC3)C=C4C(=O)O')
+# # load some molecule
+# from ase.data.pubchem import pubchem_atoms_search, pubchem_atoms_conformer_search
+# atoms = pubchem_atoms_search(smiles='N1CCN(CC1)C(C(F)=C2)=CC(=C2C4=O)N(C3CC3)C=C4C(=O)O')
 
-custom_style = amp.styles.standard
-custom_style['rotation'] = ''
-custom_style['canvas_width'] = 500
+# custom_style = amp.styles.standard
+# custom_style['rotation'] = ''
+# custom_style['canvas_width'] = 500
 
-# make some single images:
-amp.makeImage('amp',atoms,**amp.styles.standard)
-amp.makePovImage('pov',atoms,**amp.styles.standard)
+# # make some single images:
+# amp.makeImage('amp',atoms,**amp.styles.standard)
+# amp.makePovImage('pov',atoms,**amp.styles.standard)
+
+# system = amp.parsePDB("AT_PBE_GOLD.pdb")
+system = amp.parsePDB("DNA9_AT_TER.pdb")
+
+import cs4qb
+
+# ff,system = cs4qb.buildGulpFF("AT_PBE_GOLD.pdb",output_ff="custom.ff",output_cjson="system.cjson",output_psf="custom.psf")
+
+# system.check_indices()
+
+amp.write("test.pdb",system)
