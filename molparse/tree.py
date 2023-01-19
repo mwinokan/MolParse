f7956fcb1a4639651191f5d759e3ded11fb5f424
Max Winokan <mwinokan@me.com>
2023-01-19 17:32:51 +0000

    Scrolling, colors, & more

--- molparse/tree.py
+++ molparse/tree.py
@@ -7,6 +7,8 @@ from .system import System
 from .chain import Chain
 from .residue import Residue
 from .atom import Atom
+from .amino import AminoAcid
+from .nucleic import NucleicAcid
 
 def tree(obj):
 	
@@ -18,6 +20,10 @@ class TreeViewer(CursesApp):
 	def __init__(self, obj):
 		
 		super(TreeViewer, self).__init__()
+
+		if isinstance(obj,str):
+			from .io import parse
+			obj = parse(obj)
 
 		self._obj = obj
 
@@ -47,7 +53,13 @@ class TreeViewer(CursesApp):
 		except curses.error:
 			self.close()
 			print()
+			import mout
 			mout.errorOut("TreeViewer exited due to a curses error.")
+		# except KeyError:
+		# 	self.close()
+		# 	print()
+		# 	import mout
+		# 	mout.errorOut("TreeViewer exited due to a KeyError (maybe plot3d?).")
 
 		self.close()
 
@@ -70,8 +82,7 @@ class TreeViewer(CursesApp):
 
 	def object_line(self,obj,line,depth):
 
-		text = Text(f'{self.type_str(obj)}: ',line,depth*2)
-		self.add_text(text)
+		col = 4*depth
 
 		if not isinstance(obj,Atom):
 
@@ -79,41 +90,115 @@ class TreeViewer(CursesApp):
 			f2 = lambda x: x.collapse()
 
 			button = Button(app=self,
-							name=f' {obj} ',
 							line=line,
-							col=text.endcol,
+							col=col,
 							active=obj._expand,
 							enabler=f1,
+							color_active=self.RED_INV,
 							disabler=f2,
-							target=obj)
+							target=obj,
+							name=f' > ',
+							activename=f' v ')
 			self.add_button(button)
-			col = button.endcol
+			col = button.endcol + 1
 
-		else:
+		text = Text(f'{self.type_str(obj)} ',line,col,bold=True)
+		self.add_text(text)
 
-			text = Text(name=f'{obj}',
-						line=line,
-						col=text.endcol)
+		if not isinstance(obj,System):
+			text = Text(f'[',line,text.endcol,bold=True)
 			self.add_text(text)
+			text = Text(f'{obj.index}',line,text.endcol,color_pair=self.GREEN,bold=True)
+			self.add_text(text)
+			text = Text(f'] ',line,text.endcol,bold=True)
+			self.add_text(text)
+
+		if self.type_str(obj) not in ['Atom']:
+			text = Text(f'{obj.name} ',line,text.endcol,color_pair=self.CYAN,bold=True)
+			self.add_text(text)
+		else:
+			text = Text(f'{obj.symbol} ',line,text.endcol,color_pair=self.CYAN,bold=True)
+			self.add_text(text)
+
+		if self.type_str(obj) not in ['System','Atom']:
+			text = Text(f'(',line,text.endcol,bold=True)
+			self.add_text(text)
+			text = Text(f'{obj.type}',line,text.endcol,color_pair=self.MAGENTA,bold=True)
+			self.add_text(text)
+			text = Text(f') ',line,text.endcol,bold=True)
+			self.add_text(text)
+
+		if self.type_str(obj) in ['Atom']:
+
+			text = Text(f'[{obj.x:.3f}, {obj.y:.3f}, {obj.z:.3f}]',line,text.endcol,bold=True)
+			self.add_text(text)
+
+			text = Text(f' {obj.name}',line,text.endcol,color_pair=self.MAGENTA,bold=True)
+			self.add_text(text)
+
+		if self.scr_w >= 80:
+			col = self.extra_info(obj, line, text.endcol)
+		else:
 			col = text.endcol
 
-		self.extra_info(obj, line, col)
+		if self.type_str(obj) in ['System','Residue', 'AminoAcid', 'NucleicAcid']:
+
+			f1 = lambda x: x.plot3d()
+
+			button = Button(app=self,
+							line=line,
+							col=col,
+							active=False,
+							color_inactive=curses.A_UNDERLINE|curses.A_BOLD,
+							enabler=f1,
+							disabler=f1,
+							target=obj,
+							name=f'plotly')
+			
+			self.add_button(button)
+			col = button.endcol + 1
+			
+			f1 = lambda x: x.view()
+
+			button = Button(app=self,
+							line=line,
+							col=col,
+							active=False,
+							color_inactive=curses.A_UNDERLINE|curses.A_BOLD,
+							enabler=f1,
+							disabler=f1,
+							target=obj,
+							name=f'ASE')
+
+			self.add_button(button)
+			col = button.endcol + 1
 
 		return line + 1
 
 	def extra_info(self,obj,line,col):
 		if isinstance(obj,System):
-			text = Text(f' #chains={obj.num_chains}, #residues={obj.num_residues}, #atoms={obj.num_atoms}',line,col)
+			text = Text(f'contains: {self.quantity_str(obj.num_chains,"chain")}, {self.quantity_str(obj.num_residues,"residue")}, {self.quantity_str(obj.num_atoms,"atom")} ',line,col)
 			self.add_text(text)
 		elif isinstance(obj,Chain):
-			text = Text(f' #residues={obj.num_residues}, #atoms={obj.num_atoms}',line,col)
+			text = Text(f'contains: {self.quantity_str(obj.num_residues,"residue")}, {self.quantity_str(obj.num_atoms,"atom")} ',line,col)
 			self.add_text(text)
 		elif isinstance(obj,Residue):
-			text = Text(f' #atoms={obj.num_atoms}',line,col)
+			text = Text(f'contains: {self.quantity_str(obj.num_atoms,"atom")} ',line,col)
 			self.add_text(text)
+		else:
+			return col
+		return text.endcol
+
+	def quantity_str(self,number,name):
+		if number > 1:
+			return f'{number} {name}s'
+		else:
+			return f'{number} {name}'
 
 	def type_str(self,obj):
 		if isinstance(obj,System): return "System"
 		if isinstance(obj,Chain): return "Chain"
+		if isinstance(obj,AminoAcid): return "AminoAcid"
+		if isinstance(obj,NucleicAcid): return "NucleicAcid"
 		if isinstance(obj,Residue): return "Residue"
 		if isinstance(obj,Atom): return "Atom"
