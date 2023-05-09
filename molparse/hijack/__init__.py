305cb85e4b5869e25766d0456f5d52efe5d42f65
Max Winokan <mwinokan@me.com>
2023-05-09 12:00:38 +0100

    Hijack __init__

---
+++ molparse/hijack/__init__.py
@@ -0,0 +1,4 @@
+
+"""FakeAtoms and FakeSurfaceCalculator to run ASE optimisations and dynamics within an N-dimensional PMF/energy surface"""
+
+from .hijack import FakeAtoms, FakeSurfaceCalculator, NaNEncounteredInPmfError
