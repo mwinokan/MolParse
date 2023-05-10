137f35e24b6af4ff875aa51919747a90c873b67a
Max Winokan <mwinokan@me.com>
2023-05-10 11:54:35 +0100

    Remove unsampled PMF region protections, these are now handled in the pmf() call via NaNEncounteredInPmfError

--- molparse/hijack/hijack.py
+++ molparse/hijack/hijack.py
@@ -145,73 +145,34 @@ class FakeSurfaceCalculator(Calculator):
 
 		self.energy = 0.0
 
-		# print("calculate:",self.atoms.get_positions())
-
 		xs = np.array([p[0] for p in self.atoms.get_positions()])
 
 		energy = self.pmf([xs])
 		mout.debugOut(f"xs: {xs}, pmf(xs): {energy}")
-
-		# if energy > 40:
-		# 	mout.warningOut("Unsampled region! Going back one step!")
-		# 	xs = self.history[-1]
-		# 	energy = self.pmf([xs])
-		# 	mout.debugOut(f"xs: {xs}, pmf(xs): {energy}")
 		
 		self.history.append(xs)
 
 		forces = []
 
 		y = self.pmf([xs])
-		if y < 40:
 
-			# starting point within bounds of surface
+		self._last_ok = [x for x in xs]
+	
+		# partial derivatives
+		for i in range(len(self.atoms)):
 
-			self._last_ok = [x for x in xs]
-		
-			# partial derivatives
-			for i in range(len(self.atoms)):
+			h = np.array([0.0 for i in range(len(self.atoms))])
+			h[i] = self.delta
+			y_h = self.pmf([xs+h])
 
-				h = np.array([0.0 for i in range(len(self.atoms))])
-				h[i] = self.delta
-				y_h = self.pmf([xs+h])
-
-				# print(f"xs+h: {xs+h}")
-				# print(f"pmf(xs+h): {y_h}")
-
-				if self._warnings and y_h > 40:
-					mout.warningOut("Unsampled region!")
-					mout.out(f"{mcol.warning}xs: {xs+h}, pmf(xs): {y}")
-
-				dx = (y_h - y)/self.delta
-				if y_h > 40:
-					forces.append(-dx[0]*10)
-				else:
-					forces.append(-dx[0])
-
-		else:
-
-			if self._last_ok is None:
-				self._last_ok = [1.0044464, 1.7583452, 1.0085109, 2.2021041]
-
-			# starting point outside of surface
-			# try to set a restoring force
-			print(self._last_ok)
-
-			# direction to last known 'OK' position
-			dx = np.array(self._last_ok) - np.array(xs)
-
-			# force will be some constant multiplied by direction to the latest point
-			# forces = dx*self._restoring_force/np.linalg.norm(dx)
-			forces = dx*self._restoring_force
+			dx = (y_h - y)/self.delta
+			forces.append(-dx[0])
 
 		mout.debugOut(f"forces: {forces}")
 
 		forces = np.array([np.array([f,0.0,0.0]) for f in forces])
 
-		# print(forces)
-
 		### convert forces to correct units
 
-		self.results['energy'] = energy # / 100.0 ### now in eV
-		self.results['forces'] = forces# / 100.0
+		self.results['energy'] = energy
+		self.results['forces'] = forces
