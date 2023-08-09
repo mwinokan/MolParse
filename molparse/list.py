c3d367f98dd7adb3ea89433e434304cea66ccaf4
Max Winokan <mwinokan@me.com>
2023-08-09 13:17:39 +0100

    More detail when multiple results to unique query error

--- molparse/list.py
+++ molparse/list.py
@@ -35,6 +35,7 @@ class NamedList(UserList):
 				return None
 			elif len(matches) > 1:
 				import mout
+				mout.error(f'{self}.get_matches({name=}, {number=})')
 				mout.error("Multiple matches to a supposedly unique query!",fatal=True)
 			else:
 				return matches[0]
@@ -47,6 +48,7 @@ class NamedList(UserList):
 				return None
 			elif len(matches) > 1:
 				import mout
+				mout.error(f'{self}.get_matches({name=}, {number=})')
 				mout.error("Multiple matches to a supposedly unique query!",fatal=True)
 			else:
 				return matches[0]
