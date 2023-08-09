fe34f48fb1f72cfee60a94801aeb84b8a2440a6a
Max Winokan <mwinokan@me.com>
2023-08-09 13:12:31 +0100

    Interaction sites for AminoAcids

--- molparse/amino.py
+++ molparse/amino.py
@@ -138,8 +138,7 @@ class AminoAcid(Residue):
 		"""
 
 		if not self._backbone:
-			names = ["N","HN","CA","HA","C","O"]
-			self._backbone = self.get_atom(names,verbosity=0)
+			self._backbone = [a for a in self.get_atom(BB_NAMES,verbosity=0) if a is not None]
 
 		return self._backbone
 
@@ -148,6 +147,12 @@ class AminoAcid(Residue):
 		import numpy as np
 		for hydrogen in [a for a in self.atoms if a.symbol == 'H']:
 			others = [a for a in self.atoms if a.symbol != 'H' and a.index != hydrogen.index]
+
+			if len(others) == 0:
+				mout.var('this_residue',self.number_name_str)
+				self.plot3d()
+				# print(others)
+				raise Exception('No other atoms in residue when guessing hydrogen names')
 			other_index = np.argmin([np.linalg.norm(a - hydrogen) for a in others])
 			other = others[other_index]
 			newname = f'H{other.name}'
@@ -195,8 +200,7 @@ class AminoAcid(Residue):
 	@property
 	def sidechain_names(self):
 		if not self._sidechain_names:
-			bb_names = ["N","HN","CA","HA","C","O"]
-			self._sidechain_names = [n for n in self.atom_names() if n not in bb_names]
+			self._sidechain_names = [n for n in self.atom_names() if n not in BB_NAMES]
 
 		return self._sidechain_names
 	
@@ -205,7 +209,7 @@ class AminoAcid(Residue):
 		"""Get sidechain atoms"""
 
 		if not self._sidechain:
-			self._sidechain = self.get_atom(self.sidechain_names,verbosity=0)
+			self._sidechain = [a for a in self.get_atom(self.sidechain_names,verbosity=0) if a is not None]
 
 		return self._sidechain
 
@@ -223,6 +227,34 @@ class AminoAcid(Residue):
 	def HA(self):
 		"""Get Carbon-Alpha Hydrogen Atom"""
 		return self.get_atom("HA",verbosity=0)
+
+	@property
+	def interaction_sites(self):
+		
+		import numpy as np
+		from collections import namedtuple
+		
+		Site = namedtuple('Site',['type','atoms','np_pos','sidechain','res_name','res_number'])
+
+		sites = []
+
+		for interaction in BB_INTERACTION_SITES + INTERACTION_SITES[self.name]:
+
+			atoms = interaction['atoms']
+
+			if len(atoms) == 1:
+				atom = self.get_atom(atoms[0])
+				sidechain = atom.name in self.sidechain_names
+				site = Site(interaction['type'],[atom],atom.np_pos,sidechain,self.name,self.number)
+				sites.append(site)
+			else:
+				atoms = self.get_atom(atoms)
+				sidechain = atoms[0].name in self.sidechain_names
+				position = np.mean([a.np_pos for a in atoms],axis=0).round(3)
+				site = Site(interaction['type'],atoms,position,sidechain,self.name,self.number)
+				sites.append(site)
+
+		return sites
 
 	def mutate(self,newname):
 		"""Mutate this amino acid to another"""
@@ -253,8 +285,226 @@ class AminoAcid(Residue):
 		else:
 			import mout
 			mout.errorOut("Unsupported mutation!")
+
+"""
+
+	Collate interactions per site
+
+	Which interactions are shared among all amino acid backbones?
+
+	Auto-checking rules:
+
+		+ Hydrogen donor infers water donor
+		+ Hydrogen acceptor infers water acceptor
+		+ Most carbons should be able to form hydrophobic interactions
+		+ Can hydrogen donors also be acceptors?
+
+
+"""
+
+RES_NAMES = [
+	"ALA",
+	"ARG",
+	"ASN",
+	"ASP",
+	"CYS",
+	"GLN",
+	"GLU",
+	"GLY",
+	"HIS",
+	"HSD",
+	"HSE",
+	"HSP",
+	"ILE",
+	"LEU",
+	"LYS",
+	"MET",
+	"PHE",
+	"PRO",
+	"SER",
+	"THR",
+	"TRP",
+	"TYR",
+	"VAL",
+]
+
+BB_NAMES = ["N", "HN", "CA", "HA", "C", "O"]
+
+BB_INTERACTION_SITES = [
+	{"type": "water_donor", "atoms": ["N"], "source": "collated from ZV PLIP"},
+	{"type": "hydrogen_donor", "atoms": ["N"], "source": "collated from ZV PLIP"},
+	{"type": "water_acceptor", "atoms": ["O"], "source": "collated from ZV PLIP"},
+	{"type": "hydrogen_acceptor", "atoms": ["O"], "source": "collated from ZV PLIP"},
+]
+
+INTERACTION_SITES = {
+
+	"ALA": [
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "ZV PLIP"},
+	], 
+
+	"ASN": [
+		# {"type": "hydrophobic", "atoms": ["CB"], "source": "MPro PLIP"},
+		# {"type": "hydrophobic", "atoms": ["CG"], "source": "MPro PLIP"},
+		# {"type": "water_acceptor", "atoms": ["ND2"], "source": "MPro PLIP"},
+		# {"type": "hydrogen_donor", "atoms": ["ND2"], "source": "unsure"},
+	],
 	
-# from ASE
-COVALENT_RADII = {
-	'X': 0.2, 'H': 0.31, 'He': 0.28, 'Li': 1.28, 'Be': 0.96, 'B': 0.84, 'C': 0.76, 'N': 0.71, 'O': 0.66, 'F': 0.57, 'Ne': 0.58, 'Na': 1.66, 'Mg': 1.41, 'Al': 1.21, 'Si': 1.11, 'P': 1.07, 'S': 1.05, 'Cl': 1.02, 'Ar': 1.06, 'K': 2.03, 'Ca': 1.76, 'Sc': 1.7, 'Ti': 1.6, 'V': 1.53, 'Cr': 1.39, 'Mn': 1.39, 'Fe': 1.32, 'Co': 1.26, 'Ni': 1.24, 'Cu': 1.32, 'Zn': 1.22, 'Ga': 1.22, 'Ge': 1.2, 'As': 1.19, 'Se': 1.2, 'Br': 1.2, 'Kr': 1.16, 'Rb': 2.2, 'Sr': 1.95, 'Y': 1.9, 'Zr': 1.75, 'Nb': 1.64, 'Mo': 1.54, 'Tc': 1.47, 'Ru': 1.46, 'Rh': 1.42, 'Pd': 1.39, 'Ag': 1.45, 'Cd': 1.44, 'In': 1.42, 'Sn': 1.39, 'Sb': 1.39, 'Te': 1.38, 'I': 1.39, 'Xe': 1.4, 'Cs': 2.44, 'Ba': 2.15, 'La': 2.07, 'Ce': 2.04, 'Pr': 2.03, 'Nd': 2.01, 'Pm': 1.99, 'Sm': 1.98, 'Eu': 1.98, 'Gd': 1.96, 'Tb': 1.94, 'Dy': 1.92, 'Ho': 1.92, 'Er': 1.89, 'Tm': 1.9, 'Yb': 1.87, 'Lu': 1.87, 'Hf': 1.75, 'Ta': 1.7, 'W': 1.62, 'Re': 1.51, 'Os': 1.44, 'Ir': 1.41, 'Pt': 1.36, 'Au': 1.36, 'Hg': 1.32, 'Tl': 1.45, 'Pb': 1.46, 'Bi': 1.48, 'Po': 1.4, 'At': 1.5, 'Rn': 1.5, 'Fr': 2.6, 'Ra': 2.21, 'Ac': 2.15, 'Th': 2.06, 'Pa': 2.0, 'U': 1.96, 'Np': 1.9, 'Pu': 1.87, 'Am': 1.8, 'Cm': 1.69, 'Bk': 0.2, 'Cf': 0.2, 'Es': 0.2, 'Fm': 0.2, 'Md': 0.2, 'No': 0.2, 'Lr': 0.2, 'Rf': 0.2, 'Db': 0.2, 'Sg': 0.2, 'Bh': 0.2, 'Hs': 0.2, 'Mt': 0.2, 'Ds': 0.2, 'Rg': 0.2, 'Cn': 0.2, 'Nh': 0.2, 'Fl': 0.2, 'Mc': 0.2, 'Lv': 0.2, 'Ts': 0.2, 'Og': 0.2
+	"ASP": [
+		{"type": "hydrogen_acceptor", "atoms": ["OD2"], "source": "ZV PLIP"},
+		{"type": "water_acceptor", "atoms": ["OD2"], "source": "ZV PLIP (inferred)"},
+		{"type": "hydrogen_acceptor", "atoms": ["OD1"], "source": "ZV PLIP"},
+		{"type": "hydrogen_acceptor", "atoms": ["CG"], "source": "ZV PLIP"},
+		{"type": "water_acceptor", "atoms": ["CG"], "source": "ZV PLIP (inferred)"},
+		{"type": "salt_bridge", "atoms": ["OD1", "OD2"], "source": "ZV PLIP"},
+		{"type": "water_acceptor", "atoms": ["OD1"], "source": "ZV PLIP"},
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "ZV PLIP"},
+		{"type": "hydrogen_donor", "atoms": ["OD1"], "source": "ZV PLIP"},
+		{"type": "water_donor", "atoms": ["OD1"], "source": "ZV PLIP (inferred)"},
+	], 
+
+	"ARG": [
+		{"type": "hydrogen_donor", "atoms": ["NH1"], "source": "unsure"},
+		{"type": "hydrogen_donor", "atoms": ["NH2"], "source": "unsure"},
+		{"type": "water_donor", "atoms": ["NH1"], "source": "unsure"},
+		{"type": "water_donor", "atoms": ["NH2"], "source": "unsure"},
+		{"type": "salt_bridge", "atoms": ["NH1","NH2"], "source": "unsure"},
+		{"type": "pi_cation", "atoms": ["NH1"], "source": "unsure"},
+		{"type": "pi_cation", "atoms": ["NH2"], "source": "unsure"},
+	],
+
+	"CYS": [
+		{"type": "pi_cation", "atoms": ["SG"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "unsure"},
+	],
+
+	"GLN": [
+		# {"type": "hydrophobic", "atoms": ["CB"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CG"], "source": "ZV PLIP"},
+		# {"type": "hydrogen_donor", "atoms": ["NE2"], "source": "unsure"},
+		# {"type": "hydrogen_acceptor", "atoms": ["OE1"], "source": "unsure"},
+		# {"type": "halogen_acceptor", "atoms": ["O2"], "source": "MPro PLIP"},
+	],
+
+	"GLU": [
+		{"type": "hydrogen_donor", "atoms": ["OE1"], "source": "ZV PLIP"},
+		{"type": "water_donor", "atoms": ["OE1"], "source": "ZV PLIP (inferred)"},
+		# {"type": "hydrogen_donor", "atoms": ["CA"], "source": "MPro PLIP"},
+		{"type": "hydrogen_acceptor", "atoms": ["OE1"], "source": "unsure"},
+		{"type": "water_acceptor", "atoms": ["OE1"], "source": "unsure"},
+		# {"type": "hydrogen_donor", "atoms": ["N"], "source": "MPro PLIP"},
+		# {"type": "hydrophobic", "atoms": ["CB"], "source": "MPro PLIP"},
+	], 
+
+	"GLY": [
+		# {"type": "water_donor", "atoms": ["O"]},
+	], 
+
+	"HIS": [
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "ZV PLIP"},
+		{"type": "pi_stacking", "atoms": ["CG", "ND1", "CE1", "NE2", "CD2"], "source": "ZV PLIP"},
+		{"type": "pi_cation", "atoms": ["CG", "ND1", "CE1", "NE2", "CD2"], "source": "ZV PLIP (inferred)"},
+	], 
+
+	"HSE": [
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "ZV PLIP (inferred)"},
+		{"type": "pi_stacking", "atoms": ["CG", "ND1", "CE1", "NE2", "CD2"], "source": "ZV PLIP (inferred)"},
+		{"type": "pi_cation", "atoms": ["CG", "ND1", "CE1", "NE2", "CD2"], "source": "ZV PLIP (inferred)"},
+	], 
+
+	"HSD": [
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "ZV PLIP (inferred)"},
+		{"type": "pi_stacking", "atoms": ["CG", "ND1", "CE1", "NE2", "CD2"], "source": "ZV PLIP (inferred)"},
+		{"type": "pi_cation", "atoms": ["CG", "ND1", "CE1", "NE2", "CD2"], "source": "ZV PLIP (inferred)"},
+	], 
+
+	"HSP": [
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "ZV PLIP (inferred)"},
+		{"type": "pi_stacking", "atoms": ["CG", "ND1", "CE1", "NE2", "CD2"], "source": "ZV PLIP (inferred)"},
+		{"type": "pi_cation", "atoms": ["CG", "ND1", "CE1", "NE2", "CD2"], "source": "ZV PLIP (inferred)"},
+	], 
+
+	"ILE": [
+		{"type": "hydrophobic", "atoms": ["CD1"], "source": "ZV PLIP"},
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "ZV PLIP"},
+	], 
+
+	"LEU": [
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CG"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CD1"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CD2"], "source": "unsure"},
+	],
+
+	"LYS": [
+		{"type": "hydrogen_donor", "atoms": ["NZ"], "source": "unsure"},
+		{"type": "water_donor", "atoms": ["NZ"], "source": "unsure"},
+		{"type": "salt_bridge", "atoms": ["NZ"], "source": "unsure"},
+		{"type": "pi_cation", "atoms": ["NZ"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CG"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CD"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CE"], "source": "unsure"},
+	],
+	
+	"MET": [
+		{"type": "pi_cation", "atoms": ["SD"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CE"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CG"], "source": "unsure"},
+		# {"type": "hydrophobic", "atoms": ["CB"], "source": "MPro PLIP"},
+	],
+
+	"PHE": [
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CG"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CD1"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CD2"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CE1"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CE2"], "source": "unsure"},
+		{"type": "hydrophobic", "atoms": ["CZ"], "source": "unsure"},
+		{"type": "pi_stacking", "atoms": ["CG","CD1","CD2","CE1","CE2","CZ"], "source": "unsure"},
+		{"type": "pi_cation", "atoms": ["CG","CD1","CD2","CE1","CE2","CZ"], "source": "unsure"},
+	],
+
+	"PRO": [
+		{"type": "hydrophobic", "atoms": ["CG"], "source": "ZV PLIP"},
+	], 
+
+	"SER": [
+		{"type": "hydrogen_acceptor", "atoms": ["OG"], "source": "ZV PLIP"},
+		{"type": "water_acceptor", "atoms": ["OG"], "source": "ZV PLIP (inferred)"},
+		{"type": "hydrogen_donor", "atoms": ["OG"], "source": "ZV PLIP"},
+		{"type": "water_donor", "atoms": ["OG"], "source": "ZV PLIP (inferred)"},
+		# {"type": "hydrogen_donor", "atoms": ["CB"], "source": "MPro PLIP"},
+	], 
+
+	"THR": [
+		{"type": "hydrophobic", "atoms": ["CG2"], "source": "ZV PLIP"},
+		{"type": "hydrogen_donor", "atoms": ["OG1"], "source": "ZV PLIP"},
+		{"type": "water_donor", "atoms": ["OG1"], "source": "ZV PLIP (inferred)"},
+		{"type": "hydrogen_acceptor", "atoms": ["OG1"], "source": "ZV PLIP"},
+		{"type": "water_acceptor", "atoms": ["OG1"], "source": "ZV PLIP"},
+		# {"type": "water_acceptor", "atoms": ["CB"]},
+	], 
+
+	"TRP": [
+		{"type": "hydrophobic", "atoms": ["CZ2"], "source": "ZV PLIP"},
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "ZV PLIP"},
+		{"type": "pi_stacking", "atoms": ["CG", "CD1", "CD2", "NE1", "CE2"], "source": "ZV PLIP"},
+		{"type": "pi_cation", "atoms": ["CG", "CD1", "CD2", "NE1", "CE2"], "source": "ZV PLIP (inferred)"},
+	], 
+	
+	"TYR": [
+		{"type": "hydrophobic", "atoms": ["CB"], "source": "ZV PLIP"},
+		{"type": "pi_stacking", "atoms": ["CG", "CD1", "CE1", "CZ", "CE2", "CD2"], "source": "ZV PLIP"},
+		{"type": "hydrophobic", "atoms": ["CE1"], "source": "ZV PLIP"},
+		{"type": "hydrophobic", "atoms": ["CD1"], "source": "ZV PLIP"},
+		{"type": "hydrophobic", "atoms": ["CE2"], "source": "ZV PLIP"},
+		{"type": "hydrophobic", "atoms": ["CG"], "source": "ZV PLIP"},
+		{"type": "pi_cation", "atoms": ["CG", "CD1", "CE1", "CZ", "CE2", "CD2"], "source": "ZV PLIP"},
+	], 
+	
+	"VAL": [
+		{"type": "hydrophobic", "atoms": ["CG2"], "source": "ZV PLIP"},
+	], 
 }
