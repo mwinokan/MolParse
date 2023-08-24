48950cd8ca04e7dd0d8244152668fd39087412c4
Max Winokan <mwinokan@me.com>
2023-08-24 14:32:59 +0100

    features

--- molparse/rdkit/__init__.py
+++ molparse/rdkit/__init__.py
@@ -1,5 +1,5 @@
 
-from .features import features_from_group, Feature
+from .features import features_from_group, Feature, features_from_mol
 
 from .draw import draw_mol, draw_mols
 from .mol import mol_to_smiles, mol_to_pdb_block, mol_to_AtomGroup, mol_from_pdb_block, protonate
