00dff85ce559f1b65e3b855e5554fb7a5b16a106
Max Winokan <mwinokan@me.com>
2023-12-07 15:41:16 +0000

    molparse.rdkit.draw_grid

--- molparse/rdkit/__init__.py
+++ molparse/rdkit/__init__.py
@@ -1,6 +1,6 @@
 
 from .features import features_from_group, Feature, features_from_mol
 
-from .draw import draw_mol, draw_mols
+from .draw import draw_mol, draw_mols, draw_grid
 from .mol import mol_to_smiles, mol_to_pdb_block, mol_to_AtomGroup, mol_from_pdb_block, protonate
 
--- molparse/rdkit/draw.py
+++ molparse/rdkit/draw.py
@@ -2,6 +2,7 @@
 import py3Dmol
 from rdkit import Chem
 from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem import rdFMCS, AllChem
 IPythonConsole.ipython_3d = True
 
 def draw_mol(m, feats=None, p=None, confId=-1, hydrogen=True):
@@ -32,3 +33,46 @@ def draw_mols(ms, p=None, confId=-1, hydrogen=True,colors=('cyanCarbon','redCarb
                         {'stick':{'colorscheme':colors[i%len(colors)]}})
     p.zoomTo()
     return p.show()
+
+def draw_grid(mols, labels=None, find_mcs=False, align_substructure=True):
+
+# def SmilesMCStoGridImage(smiles: list[str] or dict[str, str], align_substructure: bool = True, verbose: bool = False, **kwargs):
+
+    # for mol in mols:
+    #     if isinstance(mol, str):
+    #         mol = Chem.MolFromSmiles(mol)
+
+    mols = [Chem.MolFromSmiles(mol) if isinstance(mol, str) else mol for mol in mols]
+
+    if not labels:
+        labels = [Chem.MolToSmiles(mol) for mol in mols]
+
+    if not find_mcs:
+        return Chem.Draw.MolsToGridImage(mols, legends=labels)
+
+    else:
+
+        res = rdFMCS.FindMCS(mols)
+        # mcs_smarts = res.smartsString
+        mcs_mol = Chem.MolFromSmarts(res.smartsString)
+        smarts = res.smartsString
+        smart_mol = Chem.MolFromSmarts(smarts)
+        smarts_and_mols = [smart_mol] + mols
+
+        smarts_legend = "Max. substructure match"
+
+        legends =  [smarts_legend] + labels
+
+        matches = [""] + [mol.GetSubstructMatch(mcs_mol) for mol in mols]
+
+        subms = [x for x in smarts_and_mols if x.HasSubstructMatch(mcs_mol)]
+
+        AllChem.Compute2DCoords(mcs_mol)
+
+        if align_substructure:
+            for m in subms:
+                _ = AllChem.GenerateDepictionMatching2DStructure(m, mcs_mol)
+
+        drawing = Chem.Draw.MolsToGridImage(smarts_and_mols, highlightAtomLists=matches, legends=legends)
+
+        return drawing
