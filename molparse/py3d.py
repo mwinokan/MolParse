091ae8e81cff4bea37b8534f7a7d2c6f61ba1ea3
Max Winokan <mwinokan@me.com>
2025-08-21 11:07:57 +0200

    custom size for py3dmol

--- molparse/py3d.py
+++ molparse/py3d.py
@@ -1,12 +1,19 @@
 import py3Dmol
 
 
-def render(sys, protein="cartoon", ligand="stick", protein_color="spectrum"):
+def render(
+    sys,
+    protein="cartoon",
+    ligand="stick",
+    protein_color="spectrum",
+    width="640px",
+    height="480px",
+):
 
     from tempfile import NamedTemporaryFile
 
     # protein
-    view = py3Dmol.view(data=sys.protein_system.pdb_block)
+    view = py3Dmol.view(data=sys.protein_system.pdb_block, width=width, height=height)
     view.setStyle({protein: {"color": protein_color}})
 
     # ligands
